ecc990d5f0be34434ffe5f079ade948bf6e0ed6e
tangir31 <tangirbaxtiyorov@gmail.com>
2023-10-18 21:36:09 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+let money = 10000;
+
+let name = prompt('Как вас зовут?');
+
+if (name.toLowerCase().trim() !== 'alex') {
+  alert('Пользователь не найден');
+} else {
+  let accountNumber = prompt('Номер счета?');
+  accountNumber = parseInt(accountNumber);
+
+  if (accountNumber !== 7777) {
+    alert('Пользователь не найден');
+  } else {
+    let amountToWithdraw = prompt('Сколько обналичить?');
+    amountToWithdraw = parseInt(amountToWithdraw);
+
+    if (amountToWithdraw >= 0 && amountToWithdraw <= 10000) {
+      let remainingBalance = money - amountToWithdraw;
+      alert('Все отлично');
+      alert('Сколько я снял? ' + amountToWithdraw);
+      alert('Сколько осталось? ' + remainingBalance);
+    } else {
+      alert('Недостаточно средств');
+    }
+  }
+}
+
+
+let klab = prompt('Как вас зовут?');
+
+if (klab.toLowerCase().charAt(0) !== 'a') {
+  alert('ты не изнашей банды');
+} else {
+  let age = parseInt(prompt('Сколько вам лет?'));
+
+  if (age >= 20 && age <= 40) {
+    let money = parseInt(prompt('Сколько у вас денег?'));
+
+    if (money < 100) {
+      alert('Провалилай нишеброд');
+    } else {
+      alert('Добро пожаловать');
+    }
+  } else {
+    alert('Вход запрещен');
+  }
+}
